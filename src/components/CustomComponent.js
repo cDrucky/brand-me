e1967e129089ebc88989b2e2e0e8c340c6543d96
Caleb Druckemiller <cdruckemiller@versatilets.com>
2019-09-07 08:54:58 -0400

    Lets get started

---
+++ src/components/CustomComponent.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const CustomComponent = () => {
+    return (
+        <div>
+            <h1>Custom Route Component</h1>
+        </div>
+    );
+}
+
+export default CustomComponent;
