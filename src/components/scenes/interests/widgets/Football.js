ceb853d9d9c0974fa7dc88136793221dc89ee8db
Caleb Druckemiller <cdruckemiller@versatilets.com>
2019-09-07 16:16:50 -0400

    Add two interest cards

---
+++ src/components/scenes/interests/widgets/Football.js
@@ -0,0 +1,12 @@
+import React from "react"
+import { Card, CardHeader, CardBody } from "reactstrap"
+const Football = () => {
+	return (
+		<Card>
+			<CardHeader>Football</CardHeader>
+			<CardBody>Get some football API or build a web scraper to pull some data so I can showcase it here!</CardBody>
+		</Card>
+	)
+}
+
+export default Football
